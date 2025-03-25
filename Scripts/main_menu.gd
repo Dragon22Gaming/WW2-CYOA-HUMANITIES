c223b97426e4a697e15ee61cfa25507ceb46a99d
DragonDev22 <dragon22gaming.yt@gmail.com>
2025-03-25 14:21:57 +1030

    losing my sanity more, somehow

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -4,8 +4,14 @@ extends Control
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
+# func _on_settings_pressed():
+#	pass
+
 func death():
 	# They will suffer if they choose this option >:3
 	$"dontgo".text = "Why? Why would you want to leave? Dont GOOOOO!"
 	await get_tree().create_timer(5).timeout
+	abort()
+
+func abort():
 	get_tree().quit()
