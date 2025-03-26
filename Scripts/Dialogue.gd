ba2dfedae2a2b704b4933b7cfefc7f0ef0b7a24c
DragonDev22 <dragon22gaming.yt@gmail.com>
2025-03-26 15:21:23 +1030

    insanity

--- Scripts/Dialogue.gd
+++ Scripts/Dialogue.gd
@@ -1,7 +1,13 @@
 extends ColorRect
 
+@onready var responses = $Responses
 @onready var dialogue_box = $Dialogue_Box
 var current_dialogue: String = ""
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #when the player interacts with a person they need to responds based off of that option
 #DO IT NOW YOU LITTLE SHIT :3
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+func _ready():
+	pass
---
+++ Scripts/characters/depressed.gd
@@ -0,0 +1,9 @@
+extends character
+
+var dialogue: Dictionary[String, String] = {
+	"start": 'hello you little shit',
+	"rude greeting": 'You are very rude, KYS :3'
+}
+var responses: Dictionary = {
+	"start": ["Hello!", "Piss off", ":3"],
+}
